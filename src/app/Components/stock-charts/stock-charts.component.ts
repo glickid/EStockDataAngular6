b4e0a25060c3369d54b7af2acfa60380b46e6083
glickid <glickid@yahoo.com>
2018-11-14 13:24:28 +0200

    implement stock chart component

--- src/app/Components/stock-charts/stock-charts.component.ts
+++ src/app/Components/stock-charts/stock-charts.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import * as CanvasJS from '../../../canvasjs-2.2/canvasjs.min.js';
+
+import { UserService } from '../../Services/User/user.service';
+import { User } from '../../Services/User/user';
+import { DataService } from '../../Services/Data/data.service'
 
 @Component({
   selector: 'app-stock-charts',
@@ -7,9 +13,123 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StockChartsComponent implements OnInit {
 
-  constructor() { }
+  symbol: string = "";
+  period: string = "";
+  currentUser: User;
+  stockList: any = [];
 
-  ngOnInit() {
+  constructor(private _activeRoute: ActivatedRoute,
+    private _router: Router,
+    private _userSrv: UserService,
+    private _dataSrv: DataService) {
+    this.currentUser = this._userSrv.getActiveUser();
   }
 
+  ngOnInit() {
+    this._activeRoute.params.subscribe(params => {
+      this.symbol = params['symbol'];
+      this.period = params['period'];
+
+      // console.log(this.symbol, this.period);
+      if (this.currentUser === null) {
+        this._router.navigate(['./home']);
+      } else {
+        this.loadChart();
+      }
+    });
+  }
+
+  openStockChart(symbol, period) {
+    // $location.path("/charts/"+ symbol + "/" + period );
+    this._router.navigate(['./charts/' + symbol +  '/' + period ]);
+  }
+
+  goBack() {
+    window.history.back()
+  }
+
+  loadChart() {
+
+    let dataPoints: any = [];
+
+    var chart = new CanvasJS.Chart("chartContainer", {
+      animationEnabled: true,
+      theme: "light1", // "light1", "light2", "dark1", "dark2"
+      exportEnabled: true,
+      title: {
+        text: this.symbol
+      },
+      subtitles: [{
+        text: "Daily"
+      }],
+      axisX: {
+        interval: 1,
+        valueFormatString: "D-MM-YYYY"
+      },
+      axisY: {
+        includeZero: false,
+        prefix: "$",
+        title: "Price"
+      },
+      toolTip: {
+        content: "Date: {x}<br /><strong>Price:</strong><br />Open: {y[0]}, Close: {y[3]}<br />High: {y[1]}, Low: {y[2]}"
+      },
+      data: [{
+        type: "candlestick",
+        yValueFormatString: "$####.##",
+        dataPoints: dataPoints
+      }]
+    });
+
+    setChartInfoForStockPeriod(this.symbol, this.period, this._dataSrv);
+
+    function setChartInfoForStockPeriod(stockSymbol, period, dataSrv) {
+      dataSrv.getStockChartInfo(stockSymbol, period).then(function (response) {
+        // console.log(JSON.stringify(response.data[response.data.length-1]));
+        for (var i = 0; i < response.length; i++) {
+
+          dataPoints.push({
+            x: new Date(
+              response[i]["date"]
+            ),
+            y: [
+              parseFloat(response[i]["open"]),
+              parseFloat(response[i]["high"]),
+              parseFloat(response[i]["low"]),
+              parseFloat(response[i]["close"])
+            ]
+          });
+        }
+
+        chart.render();
+
+      }, function (err) {
+        console.log(err);
+      });
+    }
+  }
+
+  searchStock(searchStr) {
+    let stockListObj: any = {};
+    this.stockList.length = 0;
+
+    if (searchStr.length > 1) {
+      // this._dataSrv.searchStock(searchStr).then(function (response) {
+        this.stockList = this._dataSrv.searchStock(searchStr);
+        
+      // }, function (err) {
+      //   console.log(err);
+      //   this.stockList.length = 0;
+      // })
+    }
+    // else {
+    //   this.stockList.length = 0;
+    // }
+  }
+
+  showChartOfStock(name, symbol) {
+    // $location.path("/charts/" + symbol + "/1m");
+    this.stockList.length = 0;
+    this._router.navigate(['./charts/' + symbol + '/' + '1m' ]);
+  }
 }
--- src/app/Services/Data/data.service.ts
+++ src/app/Services/Data/data.service.ts
@@ -12,16 +12,16 @@ export class DataService {
 
   static stocksArr: any[] = [];
   static singletonInstance: DataService;
-  
-  constructor(private http: HttpClient) { 
+
+  constructor(private http: HttpClient) {
     if (DataService.stocksArr.length === 0) {
-        this.getStockSymboles().subscribe(response => {
-          DataService.stocksArr = response;
-        });
+      this.getStockSymboles().subscribe(response => {
+        DataService.stocksArr = response;
+      });
     }
   }
 
-  getStockSymboles() : Observable<any[]> {
+  getStockSymboles(): Observable<any[]> {
     var theUrl = "https://api.iextrading.com/1.0/ref-data/symbols";
 
     return this.http.get<any[]>(theUrl).pipe(
@@ -46,7 +46,7 @@ export class DataService {
     // var key = configSrv.getStockInfoApiKey();
     const key = "0ME2BHQ21RW7FMKX";
 
-    let theUrl: string = 
+    let theUrl: string =
       "https://www.alphavantage.co/query?function=SECTOR&apikey=" + key;
     // let reply = {};
 
@@ -61,147 +61,149 @@ export class DataService {
     //         reply = { "data": RTperformance };
     //     }
     //   });
-        
+
     // return reply;
   }
 
-getCurrencyValue(C1, C2) : Observable<object | any[]> {
-//     //var key = configSrv.getStockInfoApiKey();
-  const key = "0ME2BHQ21RW7FMKX";
+  getCurrencyValue(C1, C2): Observable<object | any[]> {
+    //     //var key = configSrv.getStockInfoApiKey();
+    const key = "0ME2BHQ21RW7FMKX";
 
     let theUrl = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=" +
-        C1 + "&to_currency=" + C2 + "&apikey=" + key;
+      C1 + "&to_currency=" + C2 + "&apikey=" + key;
 
     return this.http.get(theUrl).pipe(
       catchError(this.handleError('Could not get NDX info', [])));
-}
-//     let currencyObject = {};
+  }
+  //     let currencyObject = {};
 
-//     this.http.get(theUrl)
-//       .subscribe((data) =>
-//       {        
-//         if (data.hasOwnProperty("Realtime Currency Exchange Rate")) {
-//             currencyObject[data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]] =
-//                 data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
-//         }
-//         else {
-//             console.log("failed to parse Currency response ");
-//             console.log(data);
-//         }
-//       });
-//     // , function (err) {
-//     //     $log.error(err);
-//     //     async.reject("failed to get a currency value");
-//     // })
+  //     this.http.get(theUrl)
+  //       .subscribe((data) =>
+  //       {        
+  //         if (data.hasOwnProperty("Realtime Currency Exchange Rate")) {
+  //             currencyObject[data["Realtime Currency Exchange Rate"]["4. To_Currency Name"]] =
+  //                 data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
+  //         }
+  //         else {
+  //             console.log("failed to parse Currency response ");
+  //             console.log(data);
+  //         }
+  //       });
+  //     // , function (err) {
+  //     //     $log.error(err);
+  //     //     async.reject("failed to get a currency value");
+  //     // })
 
-//     return (currencyObject);
-// }
+  //     return (currencyObject);
+  // }
 
-// getCurrencies() {
-//     // var async = $q.defer();
-//     // var currArr = configSrv.getCurrencyArr();
-//     const currArr = ["USD", "EUR", "GBP", "JPY", "CAD", "HKD"];
+  // getCurrencies() {
+  //     // var async = $q.defer();
+  //     // var currArr = configSrv.getCurrencyArr();
+  //     const currArr = ["USD", "EUR", "GBP", "JPY", "CAD", "HKD"];
 
-//     var C1 = currArr[0];
-//     var C2 = "";
+  //     var C1 = currArr[0];
+  //     var C2 = "";
 
-//     for (var i = 1; i < currArr.length; i++) {
-//         C2 = currArr[i];
-//         $timeout(getCurrencyValue.bind(null, C1, C2),
-//             (30000 + (15000 * (i - 1))))
-//     }
+  //     for (var i = 1; i < currArr.length; i++) {
+  //         C2 = currArr[i];
+  //         $timeout(getCurrencyValue.bind(null, C1, C2),
+  //             (30000 + (15000 * (i - 1))))
+  //     }
 
-//     $q.all(premises).then(function (response) {
-//         $localStorage.currencyObject = currencyObject;
-//         async.resolve(currencyObject);
-//     }, function (error) {
-//         $log.log(error);
-//         async.reject("failed to currency values")
-//     });
+  //     $q.all(premises).then(function (response) {
+  //         $localStorage.currencyObject = currencyObject;
+  //         async.resolve(currencyObject);
+  //     }, function (error) {
+  //         $log.log(error);
+  //         async.reject("failed to currency values")
+  //     });
 
-//     return (async.promise);
-// }
+  //     return (async.promise);
+  // }
 
-  getNDX() : Observable<object | any[]> {
-//     var key = configSrv.getStockInfoApiKey();
+  getNDX(): Observable<object | any[]> {
+    //     var key = configSrv.getStockInfoApiKey();
     const stockInfoApiKey = "0ME2BHQ21RW7FMKX";
 
-    var theUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NDX&outputsize=compact&apikey=" 
-    + stockInfoApiKey;
+    var theUrl = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=NDX&outputsize=compact&apikey="
+      + stockInfoApiKey;
 
     return this.http.get(theUrl).pipe(
       catchError(this.handleError('Could not get NDX info', [])));
-   }
+  }
 
-   /**
- * Handle Http operation that failed.
- * Let the app continue.
- * @param operation - name of the operation that failed
- * @param result - optional value to return as the observable result
- */
-  private handleError<T> (operation = 'operation', result?: T) {
+  /**
+* Handle Http operation that failed.
+* Let the app continue.
+* @param operation - name of the operation that failed
+* @param result - optional value to return as the observable result
+*/
+  private handleError<T>(operation = 'operation', result?: T) {
     return (error: any): Observable<T> => {
-  
+
       console.error(error); // log to console instead
-  
+
       // this.log(`${operation} failed: ${error.message}`);
-  
+
       // Let the app keep running by returning an empty result.
       return of(result as T);
     };
   }
 
-  getGainersList() : Observable<any[]> {
+  getGainersList(): Observable<any[]> {
     var theUrl = "https://api.iextrading.com/1.0/stock/market/list/gainers";
 
     return this.http.get<any[]>(theUrl).pipe(
       catchError(this.handleError('Could not get Gainers info', [])));
   }
 
-  getLosersList() : Observable<any[]> {
+  getLosersList(): Observable<any[]> {
     var theUrl = "https://api.iextrading.com/1.0/stock/market/list/losers";
 
     return this.http.get<any[]>(theUrl).pipe(
       catchError(this.handleError('Could not get Losers info', [])));
   }
 
-  getMostActive() : Observable<any[]> {
+  getMostActive(): Observable<any[]> {
     var theUrl = "https://api.iextrading.com/1.0/stock/market/list/mostactive";
 
     return this.http.get<any[]>(theUrl).pipe(
       catchError(this.handleError('Could not get Active info', [])));
   }
 
- searchStock(searchStr) {
+  searchStock(searchStr) {
     var stockList = []
-   
+
     var lowerName = "";
     var lowerSym = "";
     var lowerStr = searchStr.toLowerCase();
 
     for (var i = 0; i < DataService.stocksArr.length; i++) {
-        lowerName = DataService.stocksArr[i].name.toLowerCase();
-        lowerSym = DataService.stocksArr[i].symbol.toLowerCase();
+      lowerName = DataService.stocksArr[i].name.toLowerCase();
+      lowerSym = DataService.stocksArr[i].symbol.toLowerCase();
 
-        if ((lowerName.includes(lowerStr)) ||
-            (lowerSym.includes(lowerStr))) {
-            stockList.push ( {"name" : DataService.stocksArr[i].name, 
-                              "symbol" : DataService.stocksArr[i].symbol});
-        }
+      if ((lowerName.includes(lowerStr)) ||
+        (lowerSym.includes(lowerStr))) {
+        stockList.push({
+          "name": DataService.stocksArr[i].name,
+          "symbol": DataService.stocksArr[i].symbol
+        });
+      }
     }
 
     return stockList;
   }
 
   getStockInfo(name: string, symbol: string, returnIndex: number) {
-    let theUrl: string = "https://api.iextrading.com/1.0/stock/" + 
-                  symbol + "/chart/1m";
+    let theUrl: string = "https://api.iextrading.com/1.0/stock/" +
+      symbol + "/chart/1m";
     // let retObj: any{} = {}
 
     // let reply: Observable<any> =  this.http.get<{}>(theUrl).pipe(
     //   catchError(this.handleError('Could not get Active info', [])));
     // let reply: {} = await this.http.get<{}>(theUrl).toPromise()
-   
+
     // let repObj: {} = reply.toPromise();
     // repObj["index"] = returnIndex;
     // return repObj;
@@ -210,24 +212,46 @@ getCurrencyValue(C1, C2) : Observable<object | any[]> {
       // note: could be written `$.get(url).done(resolve).fail(reject);`,
       //       but I expanded it out for clarity
       $.get(theUrl).done((data) => {
-          let repdata = {};
-          // infoObj = response.data;
-          var last = data[Object.keys(data)[Object.keys(data).length - 1]];
+        let repdata = {};
+        // infoObj = response.data;
+        var last = data[Object.keys(data)[Object.keys(data).length - 1]];
 
-          repdata["currentPrice"] = last["close"];
-          repdata["openPrice"] = last["open"];
-          repdata["dayVolume"] = last["volume"];
-          repdata["changePercent"] = last["changePercent"];
-          repdata["name"] = name;
-          repdata["symbol"] = symbol;
-          // stockObj["dchange"] =
-          // stockObj["overallP"] =
-          repdata["index"] = returnIndex
+        repdata["currentPrice"] = last["close"];
+        repdata["openPrice"] = last["open"];
+        repdata["dayVolume"] = last["volume"];
+        repdata["changePercent"] = last["changePercent"];
+        repdata["name"] = name;
+        repdata["symbol"] = symbol;
+        // stockObj["dchange"] =
+        // stockObj["overallP"] =
+        repdata["index"] = returnIndex
 
-          resolve(repdata);
+        resolve(repdata);
       }).fail((err) => {
-          reject(err);
+        reject(err);
       });
-  });
+    });
+  }
+
+  getStockChartInfo(symbol, timePeriod) {
+    var theUrl = "https://api.iextrading.com/1.0/stock/" + symbol + "/chart/" + timePeriod;
+
+    return new Promise<{}>((resolve, reject) => {
+      $.get(theUrl).done((data) => {
+        resolve(data);
+      }).fail((err) => {
+        reject(err);
+      });
+    });
+
+    // $http.get(theUrl).then(function (response) {
+    //     async.resolve(response);
+    // }, function (err) {
+    //     $log.error(err);
+    //     async.reject("failed to get chart info");
+    // })
+
+    // premises.push(async.promise);
+    // return async.promise;
   }
 }
