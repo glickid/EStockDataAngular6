6bd80d3b0b317a32de21a0b3af59b65db3b4ed89
glickid <glickid@yahoo.com>
2018-11-15 13:42:52 +0200

    Cont. home design + add nasdaq chart

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { HostListener } from '@angular/core';
 // import { Observable } from 'rxjs';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 
 // import { flexslider } from 'angular-flexslider';
 import { NgwWowService } from 'ngx-wow';
+import * as CanvasJS from '../../canvasjs-2.2/canvasjs.min.js';
 
 import { DataService } from '../Services/Data/data.service';
 import { interval, Subscription } from 'rxjs';
@@ -29,14 +30,14 @@ export class HomeComponent implements OnInit {
   currenciesArr = [];
   public innerHeight: any;
   subscription: Subscription;
-  activeUser : User;
-  
+  activeUser: User;
+
   constructor(private _dataService: DataService,
-              private _userSrv: UserService,
-              private _wowService: NgwWowService,
-              private _router: Router ) { 
-                this.activeUser = this._userSrv.getActiveUser();                
-              }
+    private _userSrv: UserService,
+    private _wowService: NgwWowService,
+    private _router: Router) {
+    this.activeUser = this._userSrv.getActiveUser();
+  }
 
   ngOnInit() {
     this.innerHeight = window.innerHeight;
@@ -44,7 +45,6 @@ export class HomeComponent implements OnInit {
     if (this.innerHeight > 600)
       this.quantity = 6;
 
-    
     this.getNDXinfo();
     this.getGainers();
     this.getLosers();
@@ -52,29 +52,12 @@ export class HomeComponent implements OnInit {
     this.getCurrencyValues();
     this.activeUser = this._userSrv.getActiveUser();
 
-    // this._flexslider.controller()
-    // $(".quote-slider").flexslider({
-		// 	directionNav: false
-		// });
+    $(".menu-toggle").click(function () {
+      $(this).toggleClass("active");
+      $(".main-navigation ul").toggleClass("active");
+    });
 
-		// Menu Toggle
-		$(".menu-toggle").click(function(){
-			$(this).toggleClass("active");
-			$(".main-navigation ul").toggleClass("active");
-		});
-
-		//  Replacing SVG icon with image
-		// if (!Modernizr.svg) {
-		// 	$(".svg-icon").each(function(){
-		// 		var iconNum = $(this).find("use").attr("xlink:href");
-		// 		var icon = iconNum.replace("#","");
-
-		// 		$(this).replaceWith("<img src=images/lineo-icon-pngs/"+icon+".png class=icon >");
-		// 	});
-		  
-    // }
-    
-		// WOW initiation
+    // WOW initiation
     this._wowService.init();
   }
   // ngOnDestroy() {
@@ -95,11 +78,11 @@ export class HomeComponent implements OnInit {
   isUserLoggedIn() {
     if (!this._userSrv.isLoggedIn()) {
       this.activeUser = this._userSrv.getActiveUser();
-      if (( this.activeUser === null) ||
-          (this.activeUser.id === -1) )
+      if ((this.activeUser === null) ||
+        (this.activeUser.id === -1))
         return false;
       else
-          return true;
+        return true;
     }
     this.activeUser = this._userSrv.getActiveUser();
     return true;
@@ -107,8 +90,7 @@ export class HomeComponent implements OnInit {
 
   login() {
     this._userSrv.login("yossi@yossi.com", "123")
-      .subscribe((data: User) => 
-      {
+      .subscribe((data: User) => {
         this.activeUser = this._userSrv.getActiveUser();
       });
   }
@@ -122,15 +104,18 @@ export class HomeComponent implements OnInit {
     this.NDXinfo.length = 0;
     this._dataService.getNDX().subscribe((data) => {
       let i: number = 0;
+
       if (data.hasOwnProperty("Time Series (Daily)")) {
-        let obj: any = data["Time Series (Daily)"];
-        for (let k of Object.keys(obj)) {
-          let value = obj[k];
+        let ndxDataObj: any = data["Time Series (Daily)"];
+        for (let k of Object.keys(ndxDataObj)) {
+          let value = ndxDataObj[k];
           this.NDXinfo.push({ k, value });
           i++;
           if (i === this.quantity)
             break;
         }
+
+        this.loadNdxChart(ndxDataObj);
       }
       else {
         console.log(data);
@@ -138,67 +123,115 @@ export class HomeComponent implements OnInit {
     });
   }
 
+  loadNdxChart(ndxDataObj) {
 
-  getGainers()
-  {
+    let dataPoints: any = [];
+
+    var chart = new CanvasJS.Chart("nasdaqChartContainer", {
+      animationEnabled: true,
+      theme: "light1", // "light1", "light2", "dark1", "dark2"
+      exportEnabled: true,
+      title: {
+        text: "Nasdaq 100 Index"
+      },
+      subtitles: [{
+        text: "Daily"
+      }],
+      axisX: {
+        interval: 1,
+        intervalType: "day",
+        valueFormatString: "DD-MM-YYYY"
+      },
+      axisY: {
+        includeZero: false,
+        prefix: "$",
+        title: "Price Close",
+        valueFormatString: "####"
+      },
+      data: [{
+        type: "line",
+        markerSize: 12,
+        xValueFormatString: "DD-MM-YYYY",
+        yValueFormatString: "###.#",
+        dataPoints: dataPoints
+      }]
+    });
+
+    let numItems: number = 0;
+
+    for (let key in ndxDataObj) {
+      numItems++;
+      let date = new Date(key);
+      let price = Math.floor(ndxDataObj[key]["4. close"]);
+      let obj = { "x": date, "y": price };
+
+      dataPoints.push(obj);
+      if (numItems > 11)
+        break;
+    }
+
+    chart.render();
+  }
+
+  getGainers() {
     this._dataService.getGainersList().subscribe((data) => {
-     
-      let sortedArray : any = [];
+
+      let sortedArray: any = [];
       this.gainersList.length = 0;
-      
+
       sortedArray = data.sort((obj1, obj2) => {
         if (obj1.changePercent < obj2.changePercent) {
-            return 1;
+          return 1;
         }
-    
+
         if (obj1.changePercent > obj2.changePercent) {
-            return -1;
+          return -1;
         }
-    
+
         return 0;
       });
-      this.gainersList = sortedArray.slice(0,5);
+      this.gainersList = sortedArray.slice(0, 5);
     });
   }
 
   getLosers() {
     this._dataService.getLosersList().subscribe((data) => {
-      
-      let sortedArray : any = [];
+
+      let sortedArray: any = [];
       this.loosersList.length = 0;
 
       sortedArray = data.sort((obj1, obj2) => {
         if (obj1.changePercent > obj2.changePercent) {
           return 1;
         }
-    
+
         if (obj1.changePercent < obj2.changePercent) {
-            return -1;
+          return -1;
         }
-    
+
         return 0;
       });
-      this.loosersList = sortedArray.slice(0,5);
+      this.loosersList = sortedArray.slice(0, 5);
     });
   }
 
   getMostActive() {
     this._dataService.getMostActive().subscribe((data) => {
-      let sortedArray : any = [];
+      let sortedArray: any = [];
       this.mostActiveList.length = 0;
 
       sortedArray = data.sort((obj1, obj2) => {
         if (obj1.latestVolume < obj2.latestVolume) {
-            return 1;
+          return 1;
         }
-    
+
         if (obj1.latestVolume > obj2.latestVolume) {
-            return -1;
+          return -1;
         }
-    
+
         return 0;
       });
-      this.mostActiveList = sortedArray.slice(0,5);
+      this.mostActiveList = sortedArray.slice(0, 5);
     });
   }
 
@@ -211,13 +244,13 @@ export class HomeComponent implements OnInit {
     this.currenciesArr.length = 0;
 
     for (let i = 1; i < currArr.length; i++) {
-//         $timeout(getCurrencyValue.bind(null, C1, C2),
-//             (30000 + (15000 * (i - 1))))
-//     }
-    // this.subscription = source.subscribe(val => {
+      //         $timeout(getCurrencyValue.bind(null, C1, C2),
+      //             (30000 + (15000 * (i - 1))))
+      //     }
+      // this.subscription = source.subscribe(val => {
       C2 = currArr[i];
 
-      this._dataService.getCurrencyValue(C1,C2).subscribe((data) => {
+      this._dataService.getCurrencyValue(C1, C2).subscribe((data) => {
 
         if (data.hasOwnProperty("Realtime Currency Exchange Rate")) {
           let currencyObject = {};
@@ -226,7 +259,7 @@ export class HomeComponent implements OnInit {
             data["Realtime Currency Exchange Rate"]["4. To_Currency Name"];
           currencyObject["value"] =
             data["Realtime Currency Exchange Rate"]["5. Exchange Rate"];
-          
+
           this.currenciesArr.push(currencyObject);
         }
         else {
@@ -237,7 +270,7 @@ export class HomeComponent implements OnInit {
     }
   }
 
-  changeRoute( route:string) {
+  changeRoute(route: string) {
     this._router.navigate([route]);
   }
 }
